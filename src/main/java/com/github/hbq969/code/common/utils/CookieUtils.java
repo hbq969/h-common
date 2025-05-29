2ce198884edee2f94b2c050d1f6d3fc8c2745436
hbq969 <hbq969@gmail.com>
2025-05-29 17:30:02 +0800

    CookieUtils工具类增加Cookie失效方法

--- src/main/java/com/github/hbq969/code/common/utils/CookieUtils.java
+++ src/main/java/com/github/hbq969/code/common/utils/CookieUtils.java
@@ -1,10 +1,13 @@
 package com.github.hbq969.code.common.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 public class CookieUtils {
     public static String getCookieValue(HttpServletRequest request, String key) {
         Cookie[] cookies = request.getCookies();
@@ -17,5 +20,16 @@ public class CookieUtils {
             }
         }
         return null;
+    }
+
+    public static void invalidateCookie(HttpServletResponse response, String cookieKey, String cookiePath, int httpStatusCode) {
+        Cookie jsessionCookie = new Cookie(cookieKey, null);
+        jsessionCookie.setMaxAge(0);
+        jsessionCookie.setPath(cookiePath);
+        jsessionCookie.setHttpOnly(true);
+        response.addCookie(jsessionCookie);
+        response.setStatus(httpStatusCode);
+        if (log.isDebugEnabled())
+            log.debug("设置Cookie[name={},Path={}]失效", cookieKey, cookiePath);
     }
 }
