715067acad1b632058509dfe8042c70f9d37db7e
hbq969 <hbq969@gmail.com>
2025-07-15 11:27:27 +0800

    feats: 脚本初始化辅助代码优化，增加完成检测方法

--- src/main/java/com/github/hbq969/code/common/spring/i18n/LangInfo.java
+++ src/main/java/com/github/hbq969/code/common/spring/i18n/LangInfo.java
@@ -2,10 +2,24 @@ package com.github.hbq969.code.common.spring.i18n;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 @AllArgsConstructor
 @Data
 public class LangInfo {
     private String app;
     private String lang;
+
+    public String filename(String prefix, String suffix) {
+        if (!StringUtils.endsWith(prefix, "-") && StringUtils.isNotEmpty(lang)) {
+            prefix += "-";
+        }
+        if (!StringUtils.startsWith(suffix, ".")) {
+            suffix = "." + suffix;
+        }
+        return StringUtils.isEmpty(lang) ?
+                prefix + suffix :
+                prefix + lang + suffix;
+    }
+
 }
