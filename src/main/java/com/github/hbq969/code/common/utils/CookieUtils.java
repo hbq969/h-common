c7aa6c3d814bf0e326fc5c4922f07929a6188ed6
hbq969 <hbq969@gmail.com>
2025-07-16 17:55:12 +0800

    feats: 修改CookieUtils

--- src/main/java/com/github/hbq969/code/common/utils/CookieUtils.java
+++ src/main/java/com/github/hbq969/code/common/utils/CookieUtils.java
@@ -1,11 +1,16 @@
 package com.github.hbq969.code.common.utils;
 
+import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 public class CookieUtils {
@@ -36,5 +41,11 @@ public class CookieUtils {
         response.setStatus(httpStatusCode);
         if (log.isDebugEnabled())
             log.debug("设置Cookie[name={},Path={}]失效", cookieKey, cookiePath);
+    }
+
+    public static String getSetCookie(Cookie c) {
+        String expiresHeader = FormatTime.UTC_FULL.withMills(System.currentTimeMillis() + c.getMaxAge() * 1000L);
+        return StrUtil.format("{}={};  Max-Age={}; Expires={}; Path={}; SameSite=Strict; {} {}",
+                c.getName(), c.getValue(), c.getMaxAge(), expiresHeader, c.getPath(), c.getSecure() ? "Secure;" : "", c.isHttpOnly() ? "HttpOnly;" : "");
     }
 }
