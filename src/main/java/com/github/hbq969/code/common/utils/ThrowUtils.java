5b8e3d1a0ba30d6505326dcd577e0fdcc4a1d357
hbq969 <hbq969@gmail.com>
2025-09-01 17:16:42 +0800

    ThrowUtils调整

--- src/main/java/com/github/hbq969/code/common/utils/ThrowUtils.java
+++ src/main/java/com/github/hbq969/code/common/utils/ThrowUtils.java
@@ -2,6 +2,9 @@ package com.github.hbq969.code.common.utils;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+
 @Slf4j
 public final class ThrowUtils {
     public static void call(String s, String f, Runnable r) {
@@ -10,6 +13,15 @@ public final class ThrowUtils {
             log.debug(s);
         } catch (Exception e) {
             log.info(f);
+        }
+    }
+
+    public static <V> V call(Callable<V> c, Consumer<Throwable> s) {
+        try {
+            return c.call();
+        } catch (Throwable ex) {
+            s.accept(ex);
+            return null;
         }
     }
 }
