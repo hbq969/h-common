abded8a0433f41d68de8f51c7efc239abb6796b2
hbq969 <hbq969@gmail.com>
2025-12-10 14:21:02 +0800

    feign工厂类默认支持TLS

--- src/main/java/com/github/hbq969/code/common/spring/cloud/feign/FeignFactoryBean.java
+++ src/main/java/com/github/hbq969/code/common/spring/cloud/feign/FeignFactoryBean.java
@@ -1,5 +1,6 @@
 package com.github.hbq969.code.common.spring.cloud.feign;
 
+import cn.hutool.core.util.StrUtil;
 import feign.*;
 import feign.Feign.Builder;
 import feign.codec.Decoder;
@@ -9,12 +10,28 @@ import feign.httpclient.ApacheHttpClient;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 import feign.slf4j.Slf4jLogger;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.util.Assert;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.SecureRandom;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -23,6 +40,7 @@ import java.util.List;
  * @description : 自定义Feign工厂类
  * @createTime : 2023/8/26 16:42
  */
+@Slf4j
 public class FeignFactoryBean<T> implements FactoryBean<T>, InitializingBean {
 
     private Class<T> inter;
@@ -66,7 +84,56 @@ public class FeignFactoryBean<T> implements FactoryBean<T>, InitializingBean {
     }
 
     protected Client client() {
-        return new ApacheHttpClient();
+        String url = getUrl();
+        if (StrUtil.startWith(url, "https")) {
+            return createTLSClient();
+        } else {
+            return new ApacheHttpClient();
+        }
+    }
+
+    private Client createTLSClient() {
+        try {
+            // 1. 创建信任所有证书的 TrustManager
+            X509TrustManager trustManager = new DefaultX509TrustManager();
+
+            // 2. 创建 SSLContext，使用 TLS（自动协商最佳版本）
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[]{trustManager}, new SecureRandom());
+
+            // 3. 创建 SSLConnectionSocketFactory，不限制密码套件（让系统自动协商）
+            SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(
+                    sslContext,
+                    null,  // 支持所有 TLS 协议版本，让客户端和服务端自动协商
+                    null,  // 支持所有密码套件，让客户端和服务端自动协商
+                    new NoopHostnameVerifier()
+            );
+
+            // 4. 配置连接管理器
+            Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                    .register("https", sslSocketFactory)
+                    .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                    .build();
+
+            PoolingHttpClientConnectionManager connectionManager =
+                    new PoolingHttpClientConnectionManager(registry);
+
+            // 5. 创建 HttpClient
+            HttpClient hc = HttpClientBuilder.create()
+                    .setConnectionManager(connectionManager)
+                    .setDefaultRequestConfig(RequestConfig.custom()
+                            .setConnectTimeout(10000)
+                            .setSocketTimeout(30000)
+                            .build())
+                    .setRetryHandler(new DefaultHttpRequestRetryHandler(3, true))
+                    .build();
+
+            return new ApacheHttpClient(hc);
+
+        } catch (Exception e) {
+            log.error("创建 HTTPS 客户端异常", e);
+            throw new RuntimeException("创建 HTTPS 客户端失败", e);
+        }
     }
 
     /**
