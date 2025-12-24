42ea37916b49dc2388b47ff96f5a3581931730a8
hbq969 <hbq969@gmail.com>
2025-12-24 14:06:00 +0800

    调整spring cache KEY的生成逻辑

--- src/main/java/com/github/hbq969/code/common/cache/generator/ApiKeyGenerator.java
+++ src/main/java/com/github/hbq969/code/common/cache/generator/ApiKeyGenerator.java
@@ -1,16 +1,16 @@
 package com.github.hbq969.code.common.cache.generator;
 
+import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.StrUtil;
 import com.github.hbq969.code.common.cache.Expire;
 import com.github.hbq969.code.common.cache.ExpireKey;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.interceptor.SimpleKey;
 import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.util.ClassUtils;
 
-import java.lang.reflect.Array;
 import java.lang.reflect.Method;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author : hbq969@gmail.com
@@ -22,60 +22,19 @@ public class ApiKeyGenerator extends SimpleKeyGenerator {
 
     @Override
     public Object generate(Object target, Method method, Object... params) {
-        return createKey(true, target, method, params);
+        Expire expire = AnnotationUtils.findAnnotation(method, Expire.class);
+        if (expire == null)
+            return super.generate(target, method, params);
+        String key = StrUtil.format("{}.{}", target.getClass().getName(), expire.methodKey());
+        Object[] objects = new Object[1 + ArrayUtil.length(params)];
+        objects[0] = key;
+        Object[] news = ArrayUtil.append(objects, params);
+        key = new SimpleKey(news).toString();
+        if (AnnotationUtils.findAnnotation(method, CacheEvict.class) != null)
+            log.debug("cleaning {}", key);
+        else
+            log.debug("caching {}, ({}/{})", key, expire.time(), expire.unit());
+        return new ExpireKey(key, expire.time(), expire.unit(), System.currentTimeMillis());
     }
 
-    public static Object createKey(boolean hasMethod, Object target, Method method, Object[] params) {
-        Expire expire = AnnotationUtils.findAnnotation(method, Expire.class);
-        StringBuilder sb = new StringBuilder(200);
-        sb.append(target.getClass().getName());
-        if (hasMethod) {
-            sb.append(method.getName());
-        } else if (expire != null) {
-            if (StringUtils.isNotEmpty(expire.methodKey())) {
-                sb.append('.').append(expire.methodKey());
-            }
-        }
-        sb.append("[");
-        Object param;
-        for (int i = 0; i < params.length; i++) {
-            if (i > 0) {
-                sb.append(",");
-            }
-            param = params[i];
-            if (param == null) {
-            } else if (ClassUtils.isPrimitiveArray(param.getClass())
-                    || ClassUtils.isPrimitiveWrapperArray(param.getClass())) {
-                int len = Array.getLength(param);
-                sb.append("[");
-                for (int i1 = 0; i1 < len; i1++) {
-                    if (i1 > 0) {
-                        sb.append(",");
-                    }
-                    sb.append(Array.get(param, i1));
-                }
-                sb.append("]");
-            } else if (param.getClass().isArray()) {
-                Object[] array = (Object[]) param;
-                for (int i1 = 0; i1 < array.length; i1++) {
-                    if (i1 > 0) {
-                        sb.append(",");
-                    }
-                    sb.append(array[i1]);
-                }
-            } else {
-                sb.append(param);
-            }
-        }
-        sb.append("]");
-        String key = sb.toString();
-        log.debug("接口缓存key: {}", key);
-        TimeUnit unit = TimeUnit.SECONDS;
-        long time = -1;
-        if (expire != null) {
-            unit = expire.unit();
-            time = expire.time();
-        }
-        return new ExpireKey(key, time, unit, System.currentTimeMillis());
-    }
 }
